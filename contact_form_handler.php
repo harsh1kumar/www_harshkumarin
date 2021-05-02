5753247466ef9925941111c6bbd91be9a7b82f0e
Harsh Kumar <harsh1kumar@gmail.com>
2021-05-02 08:50:46 +0530

    The first version of website, which was put on the server.

---
+++ contact_form_handler.php
@@ -0,0 +1,21 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$email_from = "contact@harshkumar.in";
+	$email_subject = "New form submission on harshkumar.in";
+	
+	$email_body = "Hi Harsh,\n\n"
+					"You have received a new message on harshkumar.in\n\n".
+					"Visitor Name: $name.\n".
+					"Visitor Email: $visitor_email.\n".
+					"Message: $message.\n\n";
+	
+	$email_to = "harshkacc1@gmail.com";
+	$headers = "From: $email_from \r\n".
+				"Reply-To: $visitor_email \r\n";
+	
+	mail($email_to, $email_subject, $email_body, $headers);
+	header("Location: index.html");
+?>
